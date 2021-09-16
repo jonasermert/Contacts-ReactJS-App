35f151edf8be24dada2b38a348fd45c1dee3bf2b
Jonas Ermert <ermert.jonas@yahoo.de>
2021-09-16 23:11:13 +0200

    added buttons to add, sort

--- src/components/actor.js
+++ src/components/actor.js
@@ -8,36 +8,13 @@ class Actor extends React.Component{
 
 render() 
     return (
+
         <div>
-            <button onClick={this.addRandom}>Add Random Contact</button>
+            <button onClick={this.addRandom}>Add Contact</button>
             <button onClick={this.sortName}>Sort by Name</button>
             <button onClick={this.sortPop}>Sort by Popularity</button>
-            <table>
-                <thead>
-                    <tr>
-                    <th>Picture</th>
-                    <th>Name</th>
-                    <th>Popularity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.actor.map(elt => 
-                            <tr>
-                                <td>
-                                    <img src={elt.pictureUrl} alt="" />
-                                </td>
-                                <td>
-                                    <p>{elt.name}</p>
-                                </td>
-                                <td>
-                                    <p>{elt.popularity.toFixed(2)}</p>
-                                </td>
-                            </tr>
-                        )}
-                </tbody>
-            </table>
-            
         </div>
+            
     )
 
 
